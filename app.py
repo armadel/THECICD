4a8e9c1e1806508a371a01c6308a7eb71bd42c6b
armadel <yacinekrima@proton.me>
2023-09-28 17:23:24 +0100

    Add existing project files to Git

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, jsonify
+import random
 
 app = Flask(__name__)
 tasks = []
+
+def generate_random_task():
+    adjectives = ['Complete', 'Create', 'Update', 'Review', 'Finish']
+    nouns = ['report', 'project', 'presentation', 'task', 'assignment']
+    return f"{random.choice(adjectives)} {random.choice(nouns)}"
 
 @app.route('/tasks', methods=['GET', 'POST'])
 def todo():
@@ -16,5 +22,7 @@ def todo():
         return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
-    # Change the port to 8080 (or any other desired port)
+    # Generate 5 random tasks at startup
+    tasks.extend([generate_random_task() for _ in range(5)])
+
     app.run(host='0.0.0.0', port=8080)
